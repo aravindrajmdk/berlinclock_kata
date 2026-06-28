c6c0bcafbce4081137e2714030d80ee91b17bd48
aravindrajmdk <79634096+aravindrajmdk@users.noreply.github.com>
2026-06-28 08:25:12 +0530

    should return O for odd seconds refactor

--- app/src/main/java/com/example/berlinclock_kata/domain/usecase/BerlinClockUseCase.kt
+++ app/src/main/java/com/example/berlinclock_kata/domain/usecase/BerlinClockUseCase.kt
@@ -1,7 +1,8 @@
 package com.example.berlinclock_kata.domain.usecase
 
 class BerlinClockUseCase {
-    fun getSecondLightColor(seconds: Int) : String{
+    fun getSecondLightColor(seconds: Int): String {
         return if (seconds % 2 == 0) "Y" else "O"
     }
+
 }
--- app/src/test/java/com/example/berlinclock_kata/BerlinClockUseCaseTest.kt
+++ app/src/test/java/com/example/berlinclock_kata/BerlinClockUseCaseTest.kt
@@ -3,20 +3,25 @@ package com.example.berlinclock_kata
 import com.example.berlinclock_kata.domain.usecase.BerlinClockUseCase
 import org.junit.Test
 import org.junit.Assert.assertEquals
+import org.junit.Before
+
 class BerlinClockUseCaseTest {
 
     lateinit var berlinClockUseCase: BerlinClockUseCase
 
+    @Before
+    fun setUp(){
+        berlinClockUseCase = BerlinClockUseCase()
+    }
+
     @Test
     fun `should return Y for even seconds`(){
-         berlinClockUseCase = BerlinClockUseCase()
         var colour=berlinClockUseCase.getSecondLightColor(2)
         assertEquals("Y",colour)
     }
 
     @Test
     fun `should return O for odd seconds`() {
-        berlinClockUseCase = BerlinClockUseCase()
         var colour = berlinClockUseCase.getSecondLightColor(1)
         assertEquals("O", colour)
     }
