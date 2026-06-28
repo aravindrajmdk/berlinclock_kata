f5d06f6fc6f83e64f73e43210cef688e8f4347e5
aravindrajmdk <79634096+aravindrajmdk@users.noreply.github.com>
2026-06-28 08:17:47 +0530

    should return Y for even seconds refactor

--- app/src/test/java/com/example/berlinclock_kata/BerlinClockUseCaseTest.kt
+++ app/src/test/java/com/example/berlinclock_kata/BerlinClockUseCaseTest.kt
@@ -5,9 +5,11 @@ import org.junit.Test
 import org.junit.Assert.assertEquals
 class BerlinClockUseCaseTest {
 
+    lateinit var berlinClockUseCase: BerlinClockUseCase
+
     @Test
     fun `should return Y for even seconds`(){
-        val berlinClockUseCase = BerlinClockUseCase()
+         berlinClockUseCase = BerlinClockUseCase()
         var colour=berlinClockUseCase.getSecondLightColor(2)
         assertEquals("Y",colour)
     }
